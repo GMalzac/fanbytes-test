5afe092d855e91a18df9358546bdddb14e6df3c1
Gregoire Malzac <gregoire.malzac@gmail.com>
2018-09-05 18:00:51 +0100

    redux promise added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 // TODO: add and export your own actions
+export function setUsers() {
+  return fetch('https://jsonplaceholder.typicode.com/users') // triggers a GET request
+   .then(response => response.json())
+   .then((data) => {
+    type: 'SET_USERS',
+    payload: data
+   })
+   .catch((error) => {
+   // Do something when something went wrong
+   }
+  ;
+}
