19e99ebe51919ebe044df1a670e87def63981877
Gregoire Malzac <gregoire.malzac@gmail.com>
2018-09-06 21:20:22 +0100

    final push

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import UserList from '../containers/user_list';
 import User from '../containers/user';
 
+
 const App = () => {
   return (
     <div className="container">
